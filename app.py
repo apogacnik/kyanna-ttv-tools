4af4fb74a255467d900989caed2c43cd3557989d
apogacnik <anzy.pog@gmail.com>
2024-10-13 14:53:07 +0200

    Logging moved & chdir

--- app.py
+++ app.py
@@ -2,12 +2,35 @@ import asyncio
 from events.event_handler import start_event_listener
 from server.server import app
 from threading import Thread
+import os
+import sys
+import logging
+from datetime import datetime
+
+
 
 # Function to run Flask in a separate thread
 def run_flask():
     app.run(host='0.0.0.0', port=5000)
 
 async def main():
+    # Change the current working directory to the script's directory
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(script_dir)
+
+    # Set up logging
+    log_folder = 'logs/'
+    os.makedirs(log_folder, exist_ok=True)  # Create the folder if it doesn't exist
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    log_filename = f'{log_folder}{current_date}.log'
+
+    logging.basicConfig(
+        filename=log_filename,
+        level=logging.INFO, 
+        format='%(asctime)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
     # Start Flask server in a separate thread
     flask_thread = Thread(target=run_flask)
     flask_thread.start()
@@ -15,6 +38,7 @@ async def main():
     # Start the EventSub listener asynchronously
     await start_event_listener()
     print("Exit...")
+    input("Press Enter to exit...")
 
 # Run the main app
 if __name__ == "__main__":
--- events/event_handler.py
+++ events/event_handler.py
@@ -9,26 +9,13 @@ from twitchAPI.object.eventsub import ChannelSubscribeEvent, ChannelFollowEvent
 from twitchAPI.type import AuthScope
 from twitchAPI.helper import first
 import requests
-
 import logging
+
 import os
-from datetime import datetime
+import sys
 
 TARGET_SCOPE = [AuthScope.CHANNEL_READ_SUBSCRIPTIONS, AuthScope.MODERATOR_READ_FOLLOWERS]
 REDIRECT_URI = 'http://localhost:17563'
-
-# Set up logging
-log_folder = 'logs/'
-os.makedirs(log_folder, exist_ok=True)  # Create the folder if it doesn't exist
-current_date = datetime.now().strftime('%Y-%m-%d')
-log_filename = f'{log_folder}{current_date}.log'
-
-logging.basicConfig(
-    filename=log_filename,
-    level=logging.INFO, 
-    format='%(asctime)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
 
 def trigger_web_alert(username, combined_audio_path):
     url = 'http://localhost:5000/alert'
@@ -65,6 +52,9 @@ async def on_follow(data: ChannelFollowEvent):
     trigger_web_alert(username, combined_audio_path)
 
 async def start_event_listener():
+    # Change the current working directory to the script's directory
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(script_dir)
     # Authentication - opens webpage to authorize the app
     twitch = await Twitch(TTV_CLIENT_ID, TTV_CLIENT_SECRET)
     helper = UserAuthenticationStorageHelper(twitch, TARGET_SCOPE)
@@ -81,7 +71,7 @@ async def start_event_listener():
     # listen to subscribtions
     sub_id = await eventsub.listen_channel_subscribe(user.id, on_subscribe)
     # listen for followers
-    follow_id = await eventsub.listen_channel_follow_v2(user.id, user.id, callback=on_follow)
+    # follow_id = await eventsub.listen_channel_follow_v2(user.id, user.id, callback=on_follow)
 
     try:
         input('press ENTER to stop\n')
